7ec6dd4a22e4cae4aa952b10c3d4f672151522bb
Michael Ishmael <michaelishmael1976@gmail.com>
2017-08-14 22:03:04 +0100

    Carousel appearing

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,5 @@ import {CardAnimator } from "./ts/CardAnimator"
 let anim = new CardAnimator();
 anim.init();
 
-document.write("Testing 3");
-console.log("Hello World");
+//document.write("Testing 3");
+//console.log("Hello World");
